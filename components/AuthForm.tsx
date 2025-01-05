407a3827d2e96bd0264198af85d03ced0a4c1750
Shaurya Singh <shaurya117aa@gmail.com>
2025-01-06 02:35:26 +0530

    Setup Appwrite and Create Account

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import React, { useContext } from "react";
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -16,9 +17,11 @@ import { Input } from "@/components/ui/input"
 import { useState } from "react"
 import Image from "next/image";
 import Link from "next/link";
+import { createAccount } from "@/lib/actions/user.actions"
 
 
-type FormType = "sign-in" | "sign-up;"
+
+type FormType = "sign-in" | "sign-up"
 
 
 const authFormSchema = (formType: FormType) => {
@@ -33,7 +36,8 @@ const authFormSchema = (formType: FormType) => {
 const AuthForm = ({type}: {type: FormType}) => {
 
     const [isLoading, setisLoading] = useState(false)
-    const [errorMessage, seterrorMessage] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+    const [accountId, setAccountId] = useState("")
 
   const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
@@ -44,9 +48,24 @@ const AuthForm = ({type}: {type: FormType}) => {
   })
  
   
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async(values: z.infer<typeof formSchema>) =>{
     
-    console.log(values)
+    setisLoading(true);
+    setErrorMessage("");
+
+    try {
+      const user = await createAccount({
+              fullName: values.fullName || "",
+              email: values.email,
+            });
+
+      setAccountId(user.accountId);
+    } catch {
+      setErrorMessage("Failed to create account. Please try again.");
+    } finally {
+      setisLoading(false);
+    }
+
   }
   return (
     <>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { unknown } from "zod"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const parseStringify = (value:unknown) => {
+  return JSON.parse(JSON.stringify(value));
 }
